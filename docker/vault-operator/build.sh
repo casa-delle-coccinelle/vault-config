e64dc9151357fc1e7b8461cffc3d9b4ba2e6e948
Ivanov Dimitar (IOC/PAP-DDM-RM) <dimitar.ivanov@bosch.io>
2022-11-23 22:35:34 +0200

    Correcting build to enable push

--- docker/vault-operator/build.sh
+++ docker/vault-operator/build.sh
@@ -6,3 +6,8 @@ export VERSION="$(grep ^version: ../../charts/vault-config/Chart.yaml | awk '{pr
 
 
 docker build --tag vault-operator:${VERSION} --build-arg "KUBECTL_VERSION=${KUBECTL_VERSION}" --build-arg "VAULT_VERSION=${VAULT_VERSION}" -f Dockerfile .
+
+if [ "${#}" == 1 ]; then
+    docker tag "vault-operator:${VERSION}" "${1}:${VERSION}"
+    docker push "${1}:${VERSION}"
+fi
